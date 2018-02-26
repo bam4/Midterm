7b59e0dca58d6c3f812e48662e433ac9e28d408a
Brett Moody <brettmoody@me.com>
2018-02-25 22:50:15 -0500

    More builds.

---
+++ Raycasting_Test/Assets/detectWall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectWall : MonoBehaviour {
+
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		var raycastHit = new RaycastHit ();
+		var distance = 10f;
+		Debug.DrawRay (transform.position, transform.forward * distance, Color.red);
+
+		var layerMask = LayerMask.GetMask (LayerMask.NameToLayer ("Game"));
+		layerMask = ~layerMask;
+
+		if (Physics.Raycast(transform.position, transform.forward, out raycastHit, distance, layerMask))
+		{
+			Debug.Log ("I hit something.");
+			if (raycastHit.collider.gameObject.name.ToLower().StartsWith("wall"))
+			{
+				Debug.Log("The wall.");
+			}
+		}
+	}
+}
